5715b2d64c4e0e5f1e75931077ea81975b4869af
Mateus Fonseca <mateus.xfl@gmail.com>
2023-05-26 00:26:48 -0300

    refact: Segregando publisher e subscriber.

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -7,28 +7,15 @@ using NetMQ.Sockets;
 Thread publisherThread = new Thread(Publisher);
 publisherThread.Start();
 
-
-List<Thread> subscriberThreads = new List<Thread>();
-
-// Iniciando diversos assinantes em threads separadas.
-foreach (Student student in StudentRepository.GetAll())
-{
-    subscriberThreads.Add(new Thread(Subscriber));
-    subscriberThreads.Last().Start(student);
-}
-
-/*foreach (Thread thread in subscriberThreads)
-{
-    thread.Join();
-}*/
-
 // publisherThread.Join();
 
 static void Publisher()
 {
+    Thread.Sleep(5000);
+
     using (var publisher = new PublisherSocket())
     {
-        Console.WriteLine("Publisher socket ON. \n");
+        Console.WriteLine($"Publisher socket ON at {DateTime.Now.ToString("HH:mm:ss.fff")} \n");
 
         publisher.Bind("tcp://*:5555");
 
@@ -46,36 +33,9 @@ static void Publisher()
 
             publisher.SendMoreFrame(topic).SendFrame(message);
 
-            Console.WriteLine($"Publisher: [{topic}] - {message} \n");
+            Console.WriteLine($"Publisher [{DateTime.Now.ToString("HH:mm:ss.fff")}]: [{topic}] - {message} \n");
 
             Thread.Sleep(2000);
-        }
-    }
-}
-
-static void Subscriber(object entity)
-{
-    Student student = (Student)entity;
-
-    using (var subscriber = new SubscriberSocket())
-    {
-        subscriber.Connect("tcp://localhost:5555");
-
-        foreach (Discipline discipline in student.DisciplinesEnrolled)
-        {
-            string topic = discipline.Name;
-
-            subscriber.Subscribe(topic);
-
-            Console.WriteLine($"Subscriber {student.Name} on {discipline.Name} channel. \n");
-        }
-
-        while (true)
-        {
-            string messageTopicReceived = subscriber.ReceiveFrameString();
-            string messageReceived = subscriber.ReceiveFrameString();
-
-            Console.WriteLine($"Subscriber: [{student.Name}] - [{messageTopicReceived}] - {messageReceived} \n");
         }
     }
 }
--- Subscriber/Program.cs
+++ Subscriber/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Entity.Entities;
+using Entity.Repository;
+using NetMQ;
+using NetMQ.Sockets;
+
+List<Thread> subscriberThreads = new List<Thread>();
+
+// Iniciando diversos assinantes em threads separadas.
+foreach (Student student in StudentRepository.GetAll())
+{
+    subscriberThreads.Add(new Thread(Subscriber));
+    subscriberThreads.Last().Start(student);
+}
+
+/*foreach (Thread thread in subscriberThreads)
+{
+    thread.Join();
+}*/
+
+static void Subscriber(object entity)
+{
+    Student student = (Student)entity;
+
+    using (var subscriber = new SubscriberSocket())
+    {
+        subscriber.Connect("tcp://localhost:5555");
+
+        foreach (Discipline discipline in student.DisciplinesEnrolled)
+        {
+            string topic = discipline.Name;
+
+            subscriber.Subscribe(topic);
+
+            Console.WriteLine($"Subscriber {student.Name} joined the channel {discipline.Name} at {DateTime.Now.ToString("HH:mm:ss.fff")}. \n");
+        }
+
+        while (true)
+        {
+            string messageTopicReceived = subscriber.ReceiveFrameString();
+            string messageReceived = subscriber.ReceiveFrameString();
+
+            Console.WriteLine($"Subscriber [{DateTime.Now.ToString("HH:mm:ss.fff")}]: [{student.Name}] - [{messageTopicReceived}] - {messageReceived} \n");
+        }
+    }
+}
